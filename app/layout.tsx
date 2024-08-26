68e5b1679bf34ccd64986d29d370cc93f13d98b8
maclinz <tafadzwamucheke@gmail.com>
2024-08-26 20:54:15 +0100

    testing

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <GTMInitializer />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){
+                w[l]=w[l]||[];
+                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+                var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),
+                dl=l!='dataLayer'?'&l='+l:'';
+                j.async=true;
+                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+                f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-NW7ZNK9Q');
+            `,
+          }}
+        />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
