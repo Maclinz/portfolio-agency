e2bab408285a3d89ab297e015a112ded8226a05b
maclinz <tafadzwamucheke@gmail.com>
2024-08-26 21:13:46 +0100

    fix: revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,22 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),
-                dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-NW7ZNK9Q');
-            `,
-          }}
-        />
+        <GTMInitializer />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
@@ -45,7 +30,7 @@ export default function RootLayout({
       <body className={rubik.className}>
         <noscript>
           <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-GTM-NW7ZNK9Q`}
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NW7ZNK9Q"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
